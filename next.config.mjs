7155cca24f5c18a80ca9098e1d68ee46c35cb84f
hiren55 <hirenkalsariya55@gmail.com>
2025-08-06 01:30:48 +0530

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,32 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "sleek-capybara-771.convex.cloud",
+      },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "patient-impala-737.convex.cloud",
+      },
+      {
+        protocol: "https",
+        hostname: "lovely-flamingo-139.convex.cloud",
+      },
+      {
+        protocol: "https",
+        hostname: "enchanted-chipmunk-207.convex.cloud",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
